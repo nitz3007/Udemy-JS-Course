46d39c11c3a0f3843d771d38b1bdda59fd260a88
njain <naveen.jain35@t-mobile.com>
2023-06-22 13:17:18 +0530

    assignment 1

---
+++ basics-01/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const defaultValue = 0;
+let currentResult = defaultValue;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+let calculationDescription = '(' + defaultValue + ' + 10) * 3 / 2 - 1';
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1/assignment.js
@@ -0,0 +1,10 @@
+const userInput1 = 5;
+let result;
+result = 18 + userInput1;
+result = result - 1;
+result = result * 3;
+result = result / 2;
+
+alert('Result: '+result);
+alert('user input 1: '+userInput1);
+
