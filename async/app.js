380b944eba0884d63123ca82c3da8ade97121b1e
nitz30 <niharikat80@gmail.com>
2023-12-21 00:59:23 +0530

    Promisifying Browser APIs and Promise chaining

---
+++ async/app.js
@@ -0,0 +1,52 @@
+const button = document.querySelector('button');
+const output = document.querySelector('p');
+
+//Promisifying navigator API
+const getPosition = (opt) => {
+  const promise = new Promise((resolve, reject)=> {
+    navigator.geolocation.getCurrentPosition((success)=>{
+      resolve(success);
+    },
+    (error)=>{
+  
+    },
+    opt);
+  });
+  return promise;
+}
+//Promisifying a Browser API: setTimeout()
+const setTimer = duration => {
+  const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('Promise resolved!');
+    }, duration);
+  });
+  return promise;
+}
+
+function trackUserHandler() {
+  //Promise chaining
+  let positionData;
+  getPosition().then(pos=>{
+    positionData = pos;
+    return setTimer(2000);
+  }).then(data => {
+    console.log(data, positionData);
+  })
+  // navigator.geolocation.getCurrentPosition(
+  //   posDetail => {
+  //     setTimer(2000).then(data => {
+  //       console.log(data, posDetail);
+  //     });
+  //   },
+  //   error => {
+  //     console.log(error)
+  //   }
+  // )
+  setTimeout(()=>{
+    console.log('Timer Done')
+  }, 0);
+  console.log('Getting position...')
+}
+
+button.addEventListener('click', trackUserHandler);
