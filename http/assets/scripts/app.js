9a7f3fb1935f5f88d98570c0ba656715c53a0bbd
nitz30 <niharikat80@gmail.com>
2023-12-25 22:52:15 +0530

    XML Http GET Request

---
+++ http/assets/scripts/app.js
@@ -0,0 +1,16 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.querySelector('#single-post');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+xhr.responseType = 'json';
+xhr.onload = function() {
+    const listOfPosts = xhr.response;
+    for(const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
+}
+xhr.send();
