2ed102dc360959c5e4f820113bac4035e1964f82
nitz30 <niharikat80@gmail.com>
2024-01-01 01:50:16 +0530

    http request Error handelling

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -10,7 +10,16 @@ const sendHttpRequest = (method, url, data) => {
         xhr.open(method, url);
         xhr.responseType = 'json';
         xhr.onload = function() {
-            resolve(xhr.response);
+            if(xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(new Error('Something went wrong!'));
+            }
+           
+        }
+
+        xhr.onerror = function() {
+            reject(new Error('Failed to send request!'));
         }
         xhr.send(JSON.stringify(data));
     });
@@ -18,14 +27,19 @@ const sendHttpRequest = (method, url, data) => {
 }
 
 const fetchPosts = async() => {
-    const listOfPosts = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
-    for(const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        postEl.querySelector('li').id = post.id;
-        listElement.append(postEl);
+    try {
+        const listOfPosts = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/pos');
+        for(const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            postEl.querySelector('li').id = post.id;
+            listElement.append(postEl);
+        }
+    } catch(error) {
+        alert(error.message);
     }
+   
 }
 
 const createPost = async(title, body) => {
