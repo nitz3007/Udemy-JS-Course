d60afb6074833762fa1355a8678c071b5b006272
nitz30 <niharikat80@gmail.com>
2024-01-01 01:29:52 +0530

    sending DELETE request

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.querySelector('#single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postLists = document.querySelector('ul');
 
 const sendHttpRequest = (method, url, data) => {
     const promise = new Promise((resolve, reject)=>{
@@ -22,6 +23,7 @@ const fetchPosts = async() => {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
+        postEl.querySelector('li').id = post.id;
         listElement.append(postEl);
     }
 }
@@ -42,4 +44,12 @@ form.addEventListener('submit', (event) => {
     const title = event.currentTarget.querySelector('#title').value;
     const body = event.currentTarget.querySelector('#content').value;
     createPost(title, body);
+})
+
+postLists.addEventListener('click', (event)=>{
+    if(event.target.tagName === 'BUTTON') {
+        const postId = event.target.closest('li').id;
+        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`)
+    }
+    console.log( );
 })
