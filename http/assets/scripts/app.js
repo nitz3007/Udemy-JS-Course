ac517c7bb37682c8aaad434186c96be0a70facff
nitz30 <niharikat80@gmail.com>
2024-01-01 23:52:03 +0530

    Fetch API

--- http/assets/scripts/app.js
+++ http/assets/scripts/app.js
@@ -4,31 +4,42 @@ const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
 const postLists = document.querySelector('ul');
 
-const sendHttpRequest = (method, url, data) => {
-    const promise = new Promise((resolve, reject)=>{
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-            if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject(new Error('Something went wrong!'));
-            }
+//Using XMLHttpRequest API
+// const sendHttpRequest = (method, url, data) => {
+//     const promise = new Promise((resolve, reject)=>{
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(method, url);
+//         xhr.responseType = 'json';
+//         xhr.onload = function() {
+//             if(xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.response);
+//             } else {
+//                 reject(new Error('Something went wrong!'));
+//             }
            
-        }
+//         }
 
-        xhr.onerror = function() {
-            reject(new Error('Failed to send request!'));
-        }
-        xhr.send(JSON.stringify(data));
+//         xhr.onerror = function() {
+//             reject(new Error('Failed to send request!'));
+//         }
+//         xhr.send(JSON.stringify(data));
+//     });
+//     return promise;
+// }
+
+//Using fetch() API
+const sendHttpRequest = (method,url,data) => {
+    return fetch(url,{
+        method: method,
+        data: JSON.stringify(data)
+    }).then(response => {
+        return response.json()
     });
-    return promise;
 }
 
 const fetchPosts = async() => {
     try {
-        const listOfPosts = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/pos');
+        const listOfPosts = await sendHttpRequest('GET','https://jsonplaceholder.typicode.com/posts');
         for(const post of listOfPosts) {
             const postEl = document.importNode(postTemplate.content, true);
             postEl.querySelector('h2').textContent = post.title.toUpperCase();
